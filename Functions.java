47bca5802a91265c5cb55a8b23590fa70fe5315d
Neliarredondo <121204694+Neliarredondo@users.noreply.github.com>
2023-06-04 21:07:25 -0400

    added to function class

--- Functions.java
+++ Functions.java
@@ -2,15 +2,39 @@ import java.util.Scanner;
 
 public class Functions {
     public static void main(String[] args) {
+        /**
+         * this function call the singChorus function
+         */
         singChorus();
+        // asking the user for width and length
+        double width = width();
+        double length = length();
+
+        double result = calculateArea(length, width);
+        System.out.println("The result is equal to: " + result);
+
+        // caltulating the tips
+        int bill = 100;
+        double tip = tipTheWaiter(bill);
+        System.out.println(tip);
+
+    }
+
+    public static double length() {
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter the length: ");
         double length = scan.nextDouble();
+        scan.close();
+        return length;
+
+    }
+
+    public static double width() {
+        Scanner scan = new Scanner(System.in);
         System.out.println("Enter the width: ");
         double width = scan.nextDouble();
-        double result = calculateArea(length, width);
-        System.out.println("The result is equal to: " + result);
         scan.close();
+        return width;
 
     }
 
@@ -18,7 +42,39 @@ public class Functions {
         System.out.println("Don't blame it on the sunshine");
     }
 
+    /**
+     * Function name: calculateArea
+     * 
+     * @param length (double)
+     * @param width  (double)
+     * @return (double)
+     *         Inside the function:
+     *         1. determing if length or width are null
+     *         2. return the area
+     * 
+     */
     public static double calculateArea(double length, double width) {
+        if (length < 0 || width < 0) {
+            System.out.println("INVALID DIMENTIONS");
+            System.exit(0);
+        }
         return length * width;
+
+    }
+
+    public static double tipTheWaiter(double bill) {
+        double tip = bill * 0.15;
+        return tip;
+
+    }
+
+    public static String explainArea(String language) {
+        switch (language) {
+            case "English":
+                return "Area equal length * width";
+            default:
+                return "No languange";
+        }
+
     }
 }
